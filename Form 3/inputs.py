5ee6dfa3107ed5d3adb8fc9c89d60255b2abadca
Donprof29 <48210324+Donprof29@users.noreply.github.com>
2023-06-19 09:20:47 +0000

    Update inputs.py

--- Form 3/inputs.py
+++ Form 3/inputs.py
@@ -4,7 +4,7 @@ Date: 19th June, 2023.
 Description: The program accepts an input from the user and
             then concatenate the Strings.
 """
-firstName = input("Please enter your first name ")
-lastName = input("Please enter your surname ")
-print(firstName, lastName)
+firstName = input("Please enter your first name ") #accepts user input
+lastName = input("Please enter your surname ") #accepts user input
+print(firstName, lastName)  #concatenates the two Strings
 
