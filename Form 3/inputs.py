772d06a9f877b43c89236210210745dd93d29d2d
Donprof29 <48210324+Donprof29@users.noreply.github.com>
2023-06-19 09:00:51 +0000

    Update inputs.py

--- Form 3/inputs.py
+++ Form 3/inputs.py
@@ -1,12 +1,10 @@
 """
 Author: Don Prof
-Date: 18th June, 2023.
+Date: 19th June, 2023.
 Description: The program accepts an input from the user and
             then concatenate the Strings.
 """
-
-firstName = input("Enter your first name ") #accepts input from user
-surname = input("Enter your surname ") #accepts input from user
-fullname = firstName + surname
-print(fullname)
+firstName = input("Please enter your first name ")
+lastName = input("Please enter your surname ")
+print(firstName, lastName)
 
