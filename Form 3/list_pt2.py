3367a9e73ebf403f67fcc6dea268d5681f2d0e2a
Donprof29 <48210324+Donprof29@users.noreply.github.com>
2023-06-28 18:55:58 +0000

    Update list_pt2.py

--- Form 3/list_pt2.py
+++ Form 3/list_pt2.py
@@ -10,6 +10,7 @@ playerNames.append("Haaland") #adds an item to the list
 print(playerNames) #outputs items in the list
 playerNames.insert(2, "Rashford") #adds an item to index 2
 print(playerNames) #outputs items in the list
+
 coachNames = ["Phestus", "Erik", "Zidane", "Ferguson" ] #creates a list
 playerNames.extend(coachNames) #joins coachNames list to playerNames list
 print(playerNames) #outputs items in the list
